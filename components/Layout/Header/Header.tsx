3fb8bd90b4a28de8ef73895bc2dc56a302565778
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2022-03-14 13:01:44 +0300

    Add header

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
 import { Width } from 'components';
 import { Logo } from './Logo';
+import { Phone, Messengers } from 'components/UI';
+import { Button } from '@mui/material';
+import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import DirectionsCarOutlinedIcon from '@mui/icons-material/DirectionsCarOutlined';
 import s from 'styles/components/Layout/Header/Header.module.scss';
 
 export const Header: FC = () => {
@@ -11,8 +16,27 @@ export const Header: FC = () => {
           <div className={s.logoBlock}>
             <Logo />
           </div>
-          <div className={s.socialsBlock}>2</div>
-          <div className={s.buttonsBlock}>3</div>
+          <div className={s.socialsBlock}>
+            <Phone className={s.phone} />
+            <div className={s.messengers}>
+              <Messengers className={s.messanger} height="46" width="46" />
+            </div>
+          </div>
+          <div className={s.buttonsBlock}>
+            <div className={s.buttonContainer}>
+              <Button className={s.button} size="small" variant="outlined">
+                <h4>Как узнать код краски автомобиля?</h4> <DirectionsCarOutlinedIcon />
+              </Button>
+            </div>
+            <div className={s.buttonContainer}>
+              <Button className={s.button} size="small" variant="outlined">
+                <h4>Найти код краски</h4> <SearchRoundedIcon />
+              </Button>
+              <Button className={s.button} size="small" variant="outlined">
+                <h4>Войти</h4> <LoginRoundedIcon />
+              </Button>
+            </div>
+          </div>
         </div>
       </Width>
     </header>
